b4ebc70ce8c4dda7d9e5593efa2ff906163dd94d
HanaaNajy <hanaanajy7@gmail.com>
2024-02-20 14:41:42 -0800

    up

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""method for student creation"""
 
 
 class Student:
-    """Represent a student."""
+    """Student obj"""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Gets a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+    def to_json(self):
         return self.__dict__
-
-    def reload_from_json(self, json):
-        """Replaces all attributes of the Student
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
